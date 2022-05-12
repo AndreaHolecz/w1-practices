d366b7157400cb606ac9f65020d6193abe0ecd72
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-12 09:10:58 +0200

    ok

---
+++ frontend/script.js
@@ -0,0 +1,23 @@
+console.log("ez az elso javascriptem")
+/*
+// a stringeket (szovegeket) idozelbe kell tenni
+"ez itt egy szoveg"
+// a dupla es sima macskakorom csak egy soros stringeket (szoveg) enged meg
+'ez itt egy masik szoveg'
+//backtickek koze rakott stringekben lehet sortores
+`ez egy tobb 
+soros szoveg (string)`
+
+4
+
+128
+// a szamokat nem kell idezojelbe rakni
+04
+
+1.13
+*/
+function myFirstFunction(){
+    console.log("ez az elso fuggvenyem belsejeben levo log")
+}
+
+myFirstFunction()
